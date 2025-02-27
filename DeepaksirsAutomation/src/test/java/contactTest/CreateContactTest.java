0d6db1adb9b5932dcbf1e04aa104a55398b10bfd
arunk <arunk@Arun>
2025-02-27 19:33:21 +0530

    DeepaksirsAutomation

---
+++ DeepaksirsAutomation/src/test/java/contactTest/CreateContactTest.java
@@ -0,0 +1,9 @@
+package contactTest;
+
+public class CreateContactTest {
+	public static void main(String[] args) {
+		
+		System.out.println("contacts page get created");
+	}
+
+}
---
+++ DeepaksirsAutomation/src/test/java/organisationTest/CreateOrganisationTest.java
@@ -0,0 +1,9 @@
+package organisationTest;
+
+public class CreateOrganisationTest {
+	public static void main(String[] args) {
+		
+		System.out.println("oganisation page created");
+	}
+
+}
