c9c626f75a563d95dff8427418fa39b4c0a3d3ad
arunk <arunk@Arun>
2025-02-28 11:09:25 +0530

    CreateContactTest get modified

--- DeepaksirsAutomation/src/test/java/contactTest/CreateContactTest.java
+++ DeepaksirsAutomation/src/test/java/contactTest/CreateContactTest.java
@@ -4,6 +4,7 @@ public class CreateContactTest {
 	public static void main(String[] args) {
 		
 		System.out.println("contacts page get created");
+	  System.out.println("all the contacts page should be created");
 	}
 
 }
