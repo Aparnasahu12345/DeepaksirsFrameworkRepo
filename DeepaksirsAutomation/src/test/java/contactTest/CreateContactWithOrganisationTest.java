c4137ac791ba25ab8079e89fcfd1635aa7502511
arunk <arunk@Arun>
2025-02-28 18:49:20 +0530

    create contact with organisation class  get created.

---
+++ DeepaksirsAutomation/src/test/java/contactTest/CreateContactWithOrganisationTest.java
@@ -0,0 +1,11 @@
+package contactTest;
+
+public class CreateContactWithOrganisationTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("contact created with organisation");
+
+	}
+
+}
